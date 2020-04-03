26a108287f46a4ec2676475dfe89180c6269e21f
Spooky-M <antonio.spoljar@gmail.com>
2020-04-04 00:19:41 +0200

    ToolsWindow and ToolsWindowFactory classes are now connected, first components are made.

--- src/main/java/fer/zavrsni/src/window/ToolsWindow.java
+++ src/main/java/fer/zavrsni/src/window/ToolsWindow.java
@@ -2,29 +2,48 @@ package fer.zavrsni.src.window;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowFactory;
 import org.jetbrains.annotations.NotNull;
 
-public class ToolsWindow implements ToolWindowFactory {
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
-    @Override
-    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
 
+public class ToolsWindow {
+    private JPanel toolsWindowContent;
+    private JCheckBox toolsCheckBox;
+    private Map<String, Integer> tools = new HashMap<>();
+
+    private static String toolsNames = "spotbugs_3.1.0_RC7, spotbugs_3.1.12, spotbugs_4.0.0_beta1, " +
+            "spotbugs_4.0.0_beta2, spotbugs_4.0.0_beta3, spotbugs_4.0.0_beta4, pmd, cpd, graudit, checkstyle";
+
+
+    public ToolsWindow(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        int i = 0;
+        for(String tool:toolsNames.split(",")) {
+            tools.put(tool.trim(), i++);
+        }
+        createComponents();
     }
 
-    @Override
-    public void init(ToolWindow window) {
-
+    private void createComponents() {
+        toolsWindowContent = new JPanel();
+        toolsWindowContent.setSize(200 ,200);
+        toolsCheckBox = new JCheckBox();
+        for(String tool : tools.keySet()) {
+            toolsCheckBox.add(new JLabel(tool));
+            toolsCheckBox.setMnemonic(tools.get(tool));
+            toolsCheckBox.setSelected(true);
+            toolsCheckBox.setVisible(true);
+        }
+        toolsWindowContent.setVisible(true);
     }
 
-    @Override
-    public boolean shouldBeAvailable(@NotNull Project project) {
-        return true;
+    public JPanel getContent() {
+        return toolsWindowContent;
     }
 
-    @Override
-    @Deprecated
-    public boolean isDoNotActivateOnStart() {
-        return false;
-    }
+
+
 }
--- src/main/java/fer/zavrsni/src/window/ToolsWindowFactory.java
+++ src/main/java/fer/zavrsni/src/window/ToolsWindowFactory.java
@@ -3,12 +3,23 @@ package fer.zavrsni.src.window;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.awt.*;
+
+
 public class ToolsWindowFactory implements ToolWindowFactory {
+
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
+        ToolsWindow toolsWindow = new ToolsWindow(project, toolWindow);
+        JComponent parent = toolWindow.getComponent();
+        parent.add(toolsWindow.getContent());
+        parent.setVisible(true);
+        parent.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
     }
 
     @Override
@@ -18,11 +29,13 @@ public class ToolsWindowFactory implements ToolWindowFactory {
 
     @Override
     public boolean shouldBeAvailable(@NotNull Project project) {
-        return false;
+        return true;
     }
 
     @Override
+    @Deprecated
     public boolean isDoNotActivateOnStart() {
         return false;
     }
+
 }
