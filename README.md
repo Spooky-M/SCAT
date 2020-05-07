b9208e42853df263c4865e193db74c1ca5ca6bf8
Spooky-M <antonio.spoljar@gmail.com>
2020-05-07 16:54:03 +0200

    README.md written

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# SCAT
-SCAT - Source Code Analysis Tool Plugin
+# SCAT - Source Code Analysis Tool Plugin
+SCAT (Static Code Analysis Tools) plugin provides a single interface for combining several open source tools which 
+perform security, coding style and other checks on Java static code in Android Studio projects. For it to work properly,
+it is necessary to clone a Python project (called PPProjekt) containing a script to your Android Studio project's root.
+For example, if there's a project named Application in home/userx/Documents folder, PPProjekt folder should be placed 
+like this: home/userx/Documents/Application/PPProjekt. Script can also be run from command line, and its documentation 
+is available on Github. In short, this plugin provides an interface for easier selection of desired tools and runs 
+the Python script. The Python script runs the selected tools, and a html report called simply "report.html" 
+is rendered in script's root directory, on location: PPProjekt/report.html. After the analysis, which runs in background,
+is finished, the popup opens with appropriate message, specifying the absolute path of generated report.
 
-Antonio Špoljar
+Open source code tools used: spotbugs, pmd/cpd, checkstyle, graudit.
+
+Antonio Špoljar, 8th of May, 2020.
--- src/main/java/fer/zavrsni/src/util/Utils.java
+++ src/main/java/fer/zavrsni/src/util/Utils.java
@@ -5,17 +5,4 @@ public class Utils {
             "spotbugs_4.0.0_beta2", "spotbugs_4.0.0_beta3", "spotbugs_4.0.0_beta4", "pmd", "cpd", "graudit", "checkstyle"};
     public static final String SHORT_README = "Select tools, enter which package you want to analyse, " +
             "and click \"analyse\" button to start.";
-    public static final String LONG_README = "SCAT (Static Code Analysis Tools) plugin provides a single interface " +
-            "for combining several open source tools which perform security, coding style and other checks on Java " +
-            "static code in Android Studio projects. For it to work properly, it is necessary to clone a script " +
-            "(called PPProjekt) to your Android Studio project's root. For example, if there's a project named " +
-            "Application in home/userx/Documents folder, PPProjekt folder should be placed like this: " +
-            "\"home/userx/Documents/Application/PPProjekt\". Script can also be run from command line, " +
-            "and its documentation is also available on Github. In short, this plugin provides an interface " +
-            "for easier selection of desired tools and runs the Python script. The Python script runs " +
-            "the selected tools, and a html report called simply \"report.html\" is rendered in script's root directory, " +
-            "on location: PPProjekt/report.html. After the analysis, which runs in background, is finished, " +
-            "the popup opens with appropriate message, specifying the absolute path of generated report.\n\n" +
-            "Open source code tools used: spotbugs, pmd/cpd, checkstyle, graudit.\n\n" +
-            "Antonio Špoljar, 8.5.2020.";
 }
